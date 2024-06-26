e5e8a7ccbe2547851757523eccf8a1ffb3eb47a4
avidito <av.leonardif@gmail.com>
2024-06-26 15:03:58 +0700

    feat(dashboard): update API for deployment

--- Services/dashboard-api/app/database/connection.py
+++ Services/dashboard-api/app/database/connection.py
@@ -7,11 +7,11 @@ from sqlalchemy.orm import sessionmaker
 from sshtunnel import SSHTunnelForwarder
 
 class Config(BaseSettings):
-    DB_HOST: str = Field(alias="DB_HOST")
-    DB_PORT: int = Field(alias="DB_PORT")
-    DB_NAME: str = Field(alias="DB_NAME")
     DB_USER: str = Field(alias="DB_USER")
     DB_PASS: SecretStr = Field(alias="DB_PASS")
+    DB_HOST: str = Field(alias="DB_HOST")
+    DB_PORT: int = Field(alias="DB_PORT")
+    DB_DB: str = Field(alias="DB_DB")
     SSH_TUNNEL_FLAG: int = Field(alias="SSH_TUNNEL_FLAG", default=0)
     SSH_HOST: Optional[str] = Field(alias="SSH_HOST", default=None)
     SSH_USER: Optional[str] = Field(alias="SSH_USER", default=None)
@@ -31,7 +31,7 @@ def session_factory(C: Config):
         )
         tunnel.start()
 
-    db_url = f"postgresql://{C.DB_USER}:{C.DB_PASS.get_secret_value()}@{C.DB_HOST}:{C.DB_PORT}/{C.DB_NAME}"
+    db_url = f"postgresql://{C.DB_USER}:{C.DB_PASS.get_secret_value()}@{C.DB_HOST}:{C.DB_PORT}/{C.DB_DB}"
     engine = create_engine(db_url)
 
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
